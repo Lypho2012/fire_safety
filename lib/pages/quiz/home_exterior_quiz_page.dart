8db7e359d25c563df7af3ba69af88af597fc0802
Lypho2012 <77904622+Lypho2012@users.noreply.github.com>
2022-07-13 14:10:34 -0700

    Update home_exterior_quiz_page.dart

--- lib/pages/quiz/home_exterior_quiz_page.dart
+++ lib/pages/quiz/home_exterior_quiz_page.dart
@@ -1,3 +1,4 @@
+import 'package:fire_safety/pages/quiz/smoke_alarms_quiz_page.dart';
 import 'package:fire_safety/pages/quiz_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
@@ -10,27 +11,89 @@ class HomeExteriorQuizPage extends StatefulWidget {
   State<HomeExteriorQuizPage> createState() => _HomeExteriorQuizPage();
 }
 class _HomeExteriorQuizPage extends State<HomeExteriorQuizPage> {
-  List<bool> isChecked = List<bool>.filled(10, false);
+  List<bool> isChecked = List<bool>.filled(4, false);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Padding(
           padding: EdgeInsets.only(top: 100),
-          child: Column(
+          child: ListView(
               children: [
-                Checkbox(
+                Padding(
+                  padding: EdgeInsets.only(right: 50),
+                  child: Align(
+                      alignment: Alignment.topRight,
+                      child: Text(counter.toString())
+                  )
+                ),
+                CheckboxListTile(
+                  title: Text(AppLocalizations.of(context)!.home_exterior_quiz_page_checkbox1),
                   checkColor: Colors.white,
                   value: isChecked[0],
                   onChanged: (bool? value) {
                     setState(() {
                       isChecked[0] = value!;
+                      if (isChecked[0]) {
+                        counter ++;
+                      } else {
+                        counter --;
+                      }
                     });
                   },
+                  controlAffinity: ListTileControlAffinity.leading
+                ),
+                CheckboxListTile(
+                    title: Text(AppLocalizations.of(context)!.home_exterior_quiz_page_checkbox2),
+                    checkColor: Colors.white,
+                    value: isChecked[1],
+                    onChanged: (bool? value) {
+                      setState(() {
+                        isChecked[1] = value!;
+                        if (isChecked[1]) {
+                          counter ++;
+                        } else {
+                          counter --;
+                        }
+                      });
+                    },
+                    controlAffinity: ListTileControlAffinity.leading
+                ),
+                CheckboxListTile(
+                    title: Text(AppLocalizations.of(context)!.home_exterior_quiz_page_checkbox3),
+                    checkColor: Colors.white,
+                    value: isChecked[2],
+                    onChanged: (bool? value) {
+                      setState(() {
+                        isChecked[2] = value!;
+                        if (isChecked[2]) {
+                          counter ++;
+                        } else {
+                          counter --;
+                        }
+                      });
+                    },
+                    controlAffinity: ListTileControlAffinity.leading
+                ),
+                CheckboxListTile(
+                    title: Text(AppLocalizations.of(context)!.home_exterior_quiz_page_checkbox4),
+                    checkColor: Colors.white,
+                    value: isChecked[3],
+                    onChanged: (bool? value) {
+                      setState(() {
+                        isChecked[3] = value!;
+                        if (isChecked[3]) {
+                          counter ++;
+                        } else {
+                          counter --;
+                        }
+                      });
+                    },
+                    controlAffinity: ListTileControlAffinity.leading
                 ),
                 TextButton(
                     onPressed: () {
                       Navigator.push(context, MaterialPageRoute(builder: (context) {
-                        return const QuizPage(title: 'QuizPage');
+                        return const SmokeAlarmsQuizPage();
                       }));
                     },
                     child: const Text('Next'))
