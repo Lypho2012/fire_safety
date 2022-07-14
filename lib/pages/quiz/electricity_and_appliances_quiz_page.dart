74de477630f144a2e1bd3793711eca2d385c189f
Lypho2012 <77904622+Lypho2012@users.noreply.github.com>
2022-07-14 16:01:43 -0700

    Update electricity_and_appliances_quiz_page.dart

--- lib/pages/quiz/electricity_and_appliances_quiz_page.dart
+++ lib/pages/quiz/electricity_and_appliances_quiz_page.dart
@@ -30,7 +30,7 @@ class _ElectricityAndAppliancesQuizPage extends State<ElectricityAndAppliancesQu
                       padding: EdgeInsets.only(right: 20),
                       child: Align(
                           alignment: Alignment.topRight,
-                          child: Text("Score: $counter / 41")
+                          child: Text("Score: $counter / $total")
                       )
                   ),
                   CheckboxListTile(
