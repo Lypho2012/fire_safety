07408a47e5b425559491d132f3397425c7884de5
Lypho2012 <77904622+Lypho2012@users.noreply.github.com>
2022-07-14 16:03:57 -0700

    Update smoke_alarms_quiz_page.dart

--- lib/pages/quiz/smoke_alarms_quiz_page.dart
+++ lib/pages/quiz/smoke_alarms_quiz_page.dart
@@ -31,7 +31,7 @@ class _SmokeAlarmsQuizPage extends State<SmokeAlarmsQuizPage> {
                       padding: EdgeInsets.only(right: 20),
                       child: Align(
                           alignment: Alignment.topRight,
-                          child: Text("Score: $counter / 41")
+                          child: Text("Score: $counter / $total")
                       )
                   ),
                   CheckboxListTile(
