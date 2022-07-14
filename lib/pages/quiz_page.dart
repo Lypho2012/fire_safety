55a6c152d1586fe88423783446823fe13537f4e1
Lypho2012 <77904622+Lypho2012@users.noreply.github.com>
2022-07-14 16:00:56 -0700

    Update quiz_page.dart

--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -1,9 +1,12 @@
+import 'package:fire_safety/pages/home_page.dart';
 import 'package:fire_safety/pages/quiz/home_exterior_quiz_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+int counter = 0;
+int total = 41;
 class QuizPage extends StatelessWidget {
   const QuizPage({Key? key, required this.title}) : super(key: key);
   final String title;
@@ -14,7 +17,14 @@ class QuizPage extends StatelessWidget {
           padding: EdgeInsets.only(top: 100),
           child: Column(
               children: [
-                Text('Quiz'),
+                TextButton(
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) {
+                        return const HomePage();
+                      }));
+                    },
+                    child: const Text('Skip quiz')),
+                Text('Quiz',textScaleFactor: 3),
                 Center(child: TextButton(
                     onPressed: () {
                       Navigator.push(context, MaterialPageRoute(builder: (context) {
