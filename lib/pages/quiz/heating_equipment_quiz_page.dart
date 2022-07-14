0191ff78db09a459ceed660eddc4445911cee397
Lypho2012 <77904622+Lypho2012@users.noreply.github.com>
2022-07-14 16:03:02 -0700

    Update heating_equipment_quiz_page.dart

--- lib/pages/quiz/heating_equipment_quiz_page.dart
+++ lib/pages/quiz/heating_equipment_quiz_page.dart
@@ -32,7 +32,7 @@ class _HeatingEquipmentQuizPage extends State<HeatingEquipmentQuizPage> {
                       padding: EdgeInsets.only(right: 20),
                       child: Align(
                           alignment: Alignment.topRight,
-                          child: Text("Score: $counter / 41")
+                          child: Text("Score: $counter / $total")
                       )
                   ),
                   CheckboxListTile(
